a71e5332b1da037a319dc5f11b2798ada1b9b859
neon <dogmanyo@gmail.com>
2024-02-11 19:17:08 -0500

    added proficient text

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -6,8 +6,7 @@ function Resume() {
             <h2>Resume</h2>
             <p>Download my resume <a href = "./resume.pdf">here</a></p>
             <ul>
-                <li>Proficency example here</li>
-                <li>Proficency example here</li>
+                <li>Proficient in Java, Python, C, C++, HTML, CSS, Javascript.</li>
             </ul>
         </section>
     );
